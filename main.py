017edc3d0634a2acf5d35ae632bf5e01ba93e9ab
esobirjon2197 <esobirjon2197@gmail.com>
2026-02-20 16:18:48 +0500

    Add multiple functions for various operations

---
+++ main.py
@@ -0,0 +1,71 @@
+# 1-m
+def salom(a):
+    print("Salom", a)
+
+salom("Sobirjon")
+
+
+# 2-m
+def raqam(a):
+    print("son", a)
+
+raqam(10)
+
+
+# 3-m
+def qoshuv(a, b):
+    print(a + b)
+
+qoshuv( 2, 1)
+
+
+# 4-m
+def ism(a, b):
+    print("Ism va yosh", a, b)
+
+ism("sobirjon", 16)
+
+
+# 5-m
+def kv(i):
+    print(i * i)
+
+kv(2)
+
+
+# 6-m
+def matn(a, b):
+    print(a * b)
+
+matn("Salom", 4)
+
+
+# 7-m
+def ayirish(a, b):
+    print(a - b)
+
+ayirish(2, 2)
+
+
+# 8-m
+def ism(a):
+    print(f"Hurmatli: {a}")
+
+ism("Bunyod")
+
+
+# 9-m
+def kop(a, b):
+    print(a * b)
+
+kop(2, 2)
+
+
+# 10-m
+def bol(a, b):
+    print(a / b)
+
+bol(2, 2)
+
+
+
